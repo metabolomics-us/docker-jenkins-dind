135f8435acae39f1fc7f4119b07607547b7e15ab
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2019-09-11 15:59:28 -0400

    fix build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t jenkins-dind --no-cache .
-docker tag docker tag jenkins-dind eros.fiehnlab.ucdavis.edu/jenkins-dind
+docker build -t jenkins-dind .
+docker tag jenkins-dind eros.fiehnlab.ucdavis.edu/jenkins-dind
 docker push eros.fiehnlab.ucdavis.edu/jenkins-dind
